d19ae8861bfe0e1d6a6fd29ab1e9cba6b0682cee
nes <892157341@qq.com>
2020-02-12 18:22:07 +0800

    excel 模板

---
+++ src/main/java/com/excel/FillDataToExcel.java
@@ -0,0 +1,314 @@
+package com.excel;
+
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.*;
+
+public class FillDataToExcel {
+
+    static Map<String, Object> map = new HashMap<>();
+
+    static {
+        map.put("{bgbh}", "test1");
+        map.put("{bgsj}", "test1");
+        map.put("{qymc}", "test1");
+
+        map.put("{zczb}", "test1");
+        map.put("{zcsj}", "test1");
+        map.put("{fddbrsfzh}", "test1");
+        map.put("{fddbr}", "test1");
+        map.put("{zcdz}", "test1");
+        map.put("{fddbrsfzh}", "test1");
+        map.put("{fxpj}", "test1");
+        map.put("{mxjy}", "test1");
+        map.put("{jyje}", "test1");
+        map.put("{jyll}", "test1");
+        map.put("{szqy}", "test1");
+        map.put("{jyzt}", "test1");
+        map.put("{sshy}", "test1");
+        map.put("{qyswpj}", "test1");
+        map.put("{frnl}", "test1");
+        map.put("{frcgbl}", "test1");
+        map.put("{frzjbgrq}", "test1");
+        map.put("{qysfss}", "test1");
+
+        map.put("{j24gykpdys}", 1);
+        map.put("{j24gyzszkpdyf}", 2);
+        map.put("{j12gylxkpw0dys}", "test1");
+        map.put("{j12gyzkpdyw0dys}", "test1");
+        map.put("{j2412gyzlxkpw0dys}", "test1");
+        map.put("{j12gyzkpdyw0dsy}", "test1");
+        map.put("{j6gyzkpdyw0dys}", "test1");
+        map.put("{j2gyzkpdyw0dys}", "test1");
+        map.put("{j12gyzkpzje}", "test1");
+
+        map.put("{a}", 1);
+        map.put("{b}", 1);
+        map.put("{c}", 1.4);
+        map.put("{d}", 1);
+
+        map.put("{a1}", 1);
+        map.put("{b1}", 1);
+        map.put("{c1}", 2.4);
+        map.put("{d1}", 1);
+    }
+
+    public static void main(String[] args) {
+        //writeExcel("doc" + File.separator + "test1.xlsx");
+        writeExcel("doc" + File.separator + "test3.xlsx", map);
+    }
+
+    public static File writeExcel(String tmpExcelPath, Map<String, Object> data) {
+        // 读取Excel文档
+        File excelFile = createNewFile(tmpExcelPath);
+        Workbook workBook;
+        try {
+            workBook = getWorkbook(excelFile);
+        } catch (IOException e1) {
+            throw new RuntimeException("excel文件格式错误");
+        }
+        FormulaEvaluator evaluator = workBook.getCreationHelper().createFormulaEvaluator();
+        //数据值填充
+        for (int i = 0, len = workBook.getNumberOfSheets(); i < len; i++) {
+            buildSheetValue(workBook.getSheetAt(i), data);
+        }
+        //执行表达式
+        for (int i = 0, len = workBook.getNumberOfSheets(); i < len; i++) {
+            evaluatorSheet(workBook.getSheetAt(i), evaluator);
+        }
+        //插入数据结束
+        try (OutputStream out = new FileOutputStream(excelFile)) {
+            workBook.write(out);
+            out.flush();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                workBook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return excelFile;
+    }
+
+    /**
+     * 表达式处理
+     *
+     * @param excelFile
+     */
+    public static void handlerEvaluatorExcel(File excelFile) {
+        // 读取Excel文档
+        Workbook workBook;
+        try {
+            workBook = getWorkbook(excelFile);
+        } catch (IOException e1) {
+            throw new RuntimeException("excel文件格式错误");
+        }
+        FormulaEvaluator evaluator = workBook.getCreationHelper().createFormulaEvaluator();
+        //执行表达式
+        for (int i = 0, len = workBook.getNumberOfSheets(); i < len; i++) {
+            evaluatorSheet(workBook.getSheetAt(i), evaluator);
+        }
+        //插入数据结束
+        try (OutputStream out = new FileOutputStream(excelFile)) {
+            workBook.write(out);
+            out.flush();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                workBook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void evaluatorSheet(Sheet sheet, FormulaEvaluator evaluator) {
+        Row row;
+        Cell cell;
+        //执行表达式
+        for (int r = sheet.getFirstRowNum(); r <= sheet.getLastRowNum(); ++r) {
+            row = sheet.getRow(r);
+            if (row == null) {
+                continue;
+            }
+            for (int colIx = row.getFirstCellNum(); colIx < row.getLastCellNum(); ++colIx) {
+                cell = row.getCell(colIx);
+                if (cell == null) {
+                    continue;
+                }
+                if (CellType.FORMULA.equals(cell.getCellTypeEnum())) {
+                    CellValue cellValue = evaluator.evaluate(cell);
+                    if (CellType.BOOLEAN.equals(cellValue.getCellTypeEnum())) {
+                        cell.setCellValue(cellValue.getBooleanValue());
+                    } else if (CellType.NUMERIC.equals(cellValue.getCellTypeEnum())) {
+                        cell.setCellValue(cellValue.getNumberValue());
+                    } else if (CellType.STRING.equals(cellValue.getCellTypeEnum())) {
+                        cell.setCellValue(cellValue.getStringValue());
+                    }
+                }
+            }
+        }
+    }
+
+    private static void buildSheetValue(Sheet sheet, Map<String, Object> data) {
+        if (MapUtils.isEmpty(data)) {
+            //不填充
+            return;
+        }
+        Row row;
+        Cell cell;
+        for (int r = sheet.getFirstRowNum(); r <= sheet.getLastRowNum(); ++r) {
+            row = sheet.getRow(r);
+            if (row == null) {
+                continue;
+            }
+            for (int colIx = row.getFirstCellNum(); colIx < row.getLastCellNum(); ++colIx) {
+                cell = row.getCell(colIx);
+                if (cell == null) {
+                    continue;
+                }
+                //只处理字符串填充key
+                if (CellType.STRING.equals(cell.getCellTypeEnum())) {
+                    String cv = cell.getStringCellValue();
+                    if (data.containsKey(cv)) {
+                        setCellValue(cell, data.get(cv));
+                    }
+                }
+            }
+        }
+    }
+
+    private static void setCellValue(Cell cell, Object value) {
+        if (value instanceof Integer) {
+            cell.setCellValue((Integer) value);
+        } else if (value instanceof Long) {
+            cell.setCellValue((Long) value);
+        } else if (value instanceof String) {
+            cell.setCellValue((String) value);
+        } else if (value instanceof Double) {
+            cell.setCellValue((Double) value);
+        } else if (value instanceof Date) {
+            cell.setCellValue((Date) value);
+        } else if (value instanceof Boolean) {
+            cell.setCellValue((Boolean) value);
+        } else if (value instanceof Calendar) {
+            cell.setCellValue((Calendar) value);
+        } else {
+            cell.setCellValue(String.valueOf(value));
+        }
+    }
+
+    /**
+     * 判断excel格式版本
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public static Workbook getWorkbook(File file) throws IOException {
+        Workbook wb = null;
+        FileInputStream in = new FileInputStream(file);
+        if (file.getName().endsWith(".xls")) { // Excel&nbsp;2003
+            wb = new HSSFWorkbook(in);
+        } else if (file.getName().endsWith("xlsx")) { // Excel 2007/2010
+            wb = new XSSFWorkbook(in);
+        }
+        return wb;
+    }
+
+    /**
+     * 判断excel格式版本
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public static Map<String, Boolean> getWorkbookFillSheet(File file) throws IOException {
+        Workbook wb = null;
+        FileInputStream in = new FileInputStream(file);
+        if (file.getName().endsWith(".xls")) { // Excel&nbsp;2003
+            wb = new HSSFWorkbook(in);
+        } else if (file.getName().endsWith("xlsx")) { // Excel 2007/2010
+            wb = new XSSFWorkbook(in);
+        } else {
+            throw new RuntimeException("excel文件错误");
+        }
+        Map<String, Boolean> fillSheet = new LinkedHashMap<>(wb.getNumberOfSheets());
+        Row row;
+        Cell cell;
+        Sheet sheet;
+        for (int i = 0, len = wb.getNumberOfSheets(); i < len; i++) {
+            sheet = wb.getSheetAt(i);
+            for (int r = sheet.getFirstRowNum(); r <= sheet.getLastRowNum(); ++r) {
+                row = sheet.getRow(r);
+                if (row == null) {
+                    continue;
+                }
+                boolean flag = false;
+                for (int colIx = row.getFirstCellNum(); colIx < row.getLastCellNum(); ++colIx) {
+                    cell = row.getCell(colIx);
+                    if (cell == null) {
+                        continue;
+                    }
+                    //只处理字符串填充key
+                    if (CellType.STRING.equals(cell.getCellTypeEnum())) {
+                        String cv = cell.getStringCellValue();
+                        if (cv != null && cv.matches("^\\{[\\d|\\w]+\\}$")) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if (flag) {
+                    fillSheet.put(sheet.getSheetName(), true);
+                    break;
+                }
+            }
+        }
+        return fillSheet;
+    }
+
+    public static File createNewFile(String path) {
+        // 读取模板，并赋值到新文件
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new RuntimeException("原模板文件不存在");
+        }
+        // 保存文件的路径
+        String realPath = file.getParent();
+        // 新的文件名
+        String newFileName;
+        // Excel&nbsp;2003
+        if (file.getName().endsWith(".xls")) {
+            newFileName = "fill-" + System.currentTimeMillis() + ".xls";
+        } else if (file.getName().endsWith("xlsx")) {
+            // Excel 2007/2010
+            newFileName = "fill-" + System.currentTimeMillis() + ".xlsx";
+        } else {
+            newFileName = "fill-" + System.currentTimeMillis() + ".xlsx";
+        }
+        // 判断路径是否存在
+        File dir = new File(realPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        // 写入到新的excel
+        File newFile = new File(realPath, newFileName);
+        try {
+            newFile.createNewFile();
+            // 复制模板到新文件
+            FileUtils.copyFile(file, newFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newFile;
+    }
+}
--- src/test/java/com/excel/test/ExcelRenderTest.java
+++ src/test/java/com/excel/test/ExcelRenderTest.java
@@ -1,6 +1,10 @@
 package com.excel.test;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
+import com.excel.FillDataToExcel;
 import com.spire.xls.Workbook;
 import com.spire.xls.Worksheet;
 import com.spire.xls.collections.WorksheetsCollection;
@@ -8,6 +12,7 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +21,7 @@ import java.util.Map;
  */
 public class ExcelRenderTest {
 
-    static Map<String, Object> map = new HashMap<String, Object>();
+    static Map<String, Object> map = new HashMap<>();
 
     static {
         map.put("bgbh", "test1");
@@ -51,26 +56,111 @@ public class ExcelRenderTest {
         map.put("j6gyzkpdyw0dys", "test1");
         map.put("j2gyzkpdyw0dys", "test1");
         map.put("j12gyzkpzje", "test1");
+
+        map.put("a", 1);
+        map.put("b", 1);
+        map.put("c", 1.4);
+        map.put("d", 1);
+
+        map.put("a1", 1);
+        map.put("b1", 1);
+        map.put("c1", 2.4);
+        map.put("d1", 1);
     }
 
-
     // 模板注意 用{} 来表示你要用的变量 如果本来就有"{","}" 特殊字符 用"\{","\}"代替
-    String excelFileTmp = "doc" + File.separator + "test1.xlsx";
-    //填充后的文件
-    String excelFileFill = "doc" + File.separator + "test1-fill.xlsx";
+    String excelFileTmp = "doc" + File.separator + "test3.xlsx";
+    String path = "doc" + File.separator + "convert" + File.separator;
+
+
+    @Test
+    public void testExcelConvert1() throws Exception {
+        //填充值
+        //方式1
+        File excelFileFill = FillDataToExcel.createNewFile(excelFileTmp);
+        Map<String, Boolean> workbookFillSheet = FillDataToExcel.getWorkbookFillSheet(excelFileFill);
+        ExcelWriter excelWriter =   EasyExcel.write(excelFileFill).withTemplate(excelFileTmp).build();
+        FillConfig build = FillConfig.builder().forceNewRow(Boolean.FALSE).build();
+        workbookFillSheet.forEach((k,v)->{
+            System.out.println("=="+k);
+//            EasyExcel.write(excelFileFill).withTemplate(excelFileTmp).sheet(k).doFill(map);
+            excelWriter.fill(map,build, EasyExcel.writerSheet(k).build());
+        });
+
+        //表达式处理
+       // FillDataToExcel.handlerEvaluatorExcel(excelFileFill);
+
+        //htmlToDoc(path, excelFileFill);
+
+    }
+
+    static Map<String, Object> data = new HashMap<>();
+
+    static {
+        data.put("{bgbh}", "test1");
+        data.put("{bgsj}", "test1");
+        data.put("{qymc}", "test1");
+
+        data.put("{zczb}", "test1");
+        data.put("{zcsj}", "test1");
+        data.put("{fddbrsfzh}", "test1");
+        data.put("{fddbr}", "test1");
+        data.put("{zcdz}", "test1");
+        data.put("{fddbrsfzh}", "test1");
+        data.put("{fxpj}", "test1");
+        data.put("{mxjy}", "test1");
+        data.put("{jyje}", "test1");
+        data.put("{jyll}", "test1");
+        data.put("{szqy}", "test1");
+        data.put("{jyzt}", "test1");
+        data.put("{sshy}", "test1");
+        data.put("{qyswpj}", "test1");
+        data.put("{frnl}", "test1");
+        data.put("{frcgbl}", "test1");
+        data.put("{frzjbgrq}", "test1");
+        data.put("{qysfss}", "test1");
+
+        data.put("{j24gykpdys}", 1);
+        data.put("{j24gyzszkpdyf}", 2);
+        data.put("{j12gylxkpw0dys}", "test1");
+        data.put("{j12gyzkpdyw0dys}", "test1");
+        data.put("{j2412gyzlxkpw0dys}", "test1");
+        data.put("{j12gyzkpdyw0dsy}", "test1");
+        data.put("{j6gyzkpdyw0dys}", "test1");
+        data.put("{j2gyzkpdyw0dys}", "test1");
+        data.put("{j12gyzkpzje}", "test1");
+
+        data.put("{a}", 1);
+        data.put("{b}", 1);
+        data.put("{c}", 1.4);
+        data.put("{d}", 1);
+
+        data.put("{a1}", 1);
+        data.put("{b1}", 1);
+        data.put("{c1}", 2.4);
+        data.put("{d1}", 1);
+    }
 
     /**
-     * 测试excel转换为HTML,pdf,image文件， 生成文件目录在项目中 doc/convert 下
+     * //方式2 填充+表达式计算
+     *
      * @throws Exception
      */
     @Test
-    public void testExcelConvert() throws Exception {
-        String path = "doc" + File.separator + "convert" + File.separator;
-        //填充值
-        EasyExcel.write(excelFileFill).withTemplate(excelFileTmp).sheet().doFill(map);
+    public void testExcelConvert2() throws Exception {
+        File excelFile = FillDataToExcel.writeExcel(excelFileTmp, data);
+      //  htmlToDoc(path, excelFile);
+    }
+
+    /**
+     * 测试excel转换为HTML,pdf,image文件， 生成文件目录在项目中 doc/convert 下
+     *
+     * @throws Exception
+     */
+    private void htmlToDoc(String path, File excelFileFill) throws Exception {
         //加载Excel工作表
         Workbook wb = new Workbook();
-        wb.loadFromFile(excelFileFill);
+        wb.loadFromStream(new FileInputStream(excelFileFill));
         //获取工作表
         WorksheetsCollection sheets = wb.getWorksheets();
         Worksheet worksheet;
@@ -85,7 +175,19 @@ public class ExcelRenderTest {
             // 生成 image
             worksheet.saveToImage(path + worksheet.getName() + File.separator + worksheet.getName() + ".png");
         }
+
+        //清空临时文件
+        //FileUtils.deleteDirectory(excelFile);
     }
 
-
 }
+
+
+
+/*
+1. Excel转HTML时，Excel中包含图形时不能转换到HTML
+2. Excel转PDF时，单一内容包含过宽会截断到下一页PDF中
+3. 使用的第三方免费版工具spire，对于低版本Excel只支持5个sheet，每个sheet最多两百行
+4. 定义Excel模板内容时使用单元格自定义名称填充方式时，poi不能读取到定义的名称，目前是使用的easyexcel中填充功能，占位符为：｛key｝
+5. Excel模板不能直接用表达式去计算占位符待填充的值（填充后的值表达式计算不到）
+ */
